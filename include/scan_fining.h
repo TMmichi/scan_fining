a7c398f44b6206199038763da59bc934f08000ce
TMmichi <ljh_0921@naver.com>
2019-08-12 23:10:44 +0900

    scan_fining (Errors not yet debugged)

---
+++ include/scan_fining.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <sensor_msgs/LaserScan.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <geometry_msgs/PointStamped.h>
+
+#include "../include/point.h"
+
+namespace scan_fining
+{
+
+class ScanFiner
+{
+public:
+  ScanFiner();
+
+private:
+  void updateParams();
+  void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan);
+  void globalcostmapCallback(const nav_msgs::OccupancyGrid::ConstPtr& costmap);
+  void processPoints(const sensor_msgs::LaserScan::ConstPtr& scan);
+  void transformToWorld();
+  void transformToScan();
+  void publishFinedScan(const sensor_msgs::LaserScan::ConstPtr& scan);
+
+  // ROS handles
+  ros::NodeHandle nh_;
+  ros::NodeHandle nh_local_;
+
+  //Subscribers, Publishers
+  ros::Subscriber scan_sub_;
+  ros::Subscriber costmap_sub_;
+  ros::Publisher  fined_scan_pub_;
+
+  //Transform class
+  tf::TransformListener tf_listener_;
+
+  // Parameters
+  std::string p_world_frame_;     // Name of the world coordinate frame
+  std::string p_scanner_frame_;   // Name of the scanner coordinate frame
+
+  std::string p_scan_topic_name_;
+  std::string p_costmap_topic_name_;
+  std::string p_output_topic_name_;
+
+  double p_max_scanner_range_;    // Restrictions on laser scanner
+
+  // Variables
+  std::vector<scan_fining::Point> initial_points_;
+  std::vector<std::vector<int>> costmap_;
+  int global_height_;
+  int global_width_;
+  float global_resolution_;
+  std::vector<scan_fining::Point> temp_points_;
+  std::vector<scan_fining::Point> costmap_points_;
+};
+
+} // namespace obstacle_detector
---
+++ src/scan_fining.cpp
@@ -0,0 +1,170 @@
+﻿#include "../include/scan_fining.h"
+
+using namespace std;
+using namespace scan_fining;
+
+ScanFiner::ScanFiner() : nh_(""), nh_local_("~") {
+  updateParams();
+
+  scan_sub_ = nh_.subscribe(p_scan_topic_name_, 10, &ScanFiner::scanCallback, this);
+  costmap_sub_ = nh_.subscribe(p_costmap_topic_name_, 10, &ScanFiner::globalcostmapCallback, this);
+  fined_scan_pub_ = nh_.advertise<sensor_msgs::LaserScan>(p_output_topic_name_, 10);
+
+  ROS_INFO("Scan Finer [OK]");
+  ros::spin();
+}
+
+
+void ScanFiner::updateParams() {
+  nh_local_.param<std::string>("world_frame", p_world_frame_, "map");
+  nh_local_.param<std::string>("scanner_frame", p_scanner_frame_, "base_scan");
+
+  nh_local_.param<std::string>("scan_name", p_scan_topic_name_, "scan");
+  nh_local_.param<std::string>("costmap_name", p_costmap_topic_name_, "costmap_node/costmap/costmap");
+  nh_local_.param<std::string>("output_name", p_output_topic_name_, "fined_scan");
+
+  nh_local_.param<double>("max_scanner_range", p_max_scanner_range_, 20.0);
+}
+
+
+void ScanFiner::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan) {
+  initial_points_.clear();
+
+  double phi = scan->angle_min - scan->angle_increment;
+
+  for (const float r : scan->ranges) {
+    phi += scan->angle_increment;
+
+    if (r >= scan->range_min && r <= scan->range_max && r <= p_max_scanner_range_)
+      initial_points_.push_back(Point::fromPolarCoords(r, phi));
+  }
+
+  if (!costmap_.empty())
+    processPoints(scan);
+}
+
+
+void ScanFiner::globalcostmapCallback(const nav_msgs::OccupancyGrid::ConstPtr& costmap_input){
+  costmap_.clear();
+  global_height_ = costmap_input->info.height;
+  global_width_ = costmap_input->info.width;
+  global_resolution_ = costmap_input->info.resolution;
+
+  for (int j=0; j<global_height_; j++){
+    <vector<int>> costmap_width_temp_(global_width_,0); //should be confirmed - temporally allocated vetor from for loop used as a data
+    for (int i=0; i<global_width_; i++){
+      costmap_width_temp_.push_back(costmap_points_[i+j*global_width_]);
+      //costmap[j][i] = costmap_points_[i+j*width];
+    }
+    costmap_.push_back(costmap_width_temp_);
+  }
+}
+
+
+void ScanFiner::processPoints(const sensor_msgs::LaserScan::ConstPtr& scan) {
+  
+  transformToWorld();
+  maskingScan();
+  transformToScan();
+
+  publishObstacles(scan);
+}
+
+
+void ScanFiner::transformToWorld() {
+  temp_points_.clear();
+
+  geometry_msgs::PointStamped point_l;  // Point in local (scanner) coordinate frame
+  geometry_msgs::PointStamped point_w;  // Point in global (world) coordinate frame
+
+  point_l.header.stamp = ros::Time::now();
+  point_l.header.frame_id = p_scanner_frame_;
+
+  point_w.header.stamp = ros::Time::now();
+  point_w.header.frame_id = p_world_frame_;
+
+  try {
+    tf_listener_.waitForTransform(p_world_frame_, p_scanner_frame_, ros::Time::now(), ros::Duration(3.0));
+
+    for (const Point& point_ : initial_points_) {
+        point_l.point.x = point_.x;
+        point_l.point.y = point_.y;
+        tf_listener_.transformPoint(p_world_frame_, point_l, point_w);
+        temp_points_.push_back(Point(point_w.point.x,point_w.point.y));
+      }
+    }
+  }
+  catch (tf::TransformException ex) {
+    ROS_ERROR("%s",ex.what());
+    ros::Duration(1.0).sleep();
+  }
+}
+
+
+void ScanFiner::maskingScan(){
+  for (Point& point_ : temp_points_){
+    int mask_i = point_.x/global_resolution_;
+    int mask_j = point_.y/global_resolution_;
+
+    if (costmap_[mask_j][mask_i] != 0){
+      point_.x = 0;
+      point_.y = 0;
+    }
+  }
+}
+
+
+void ScanFiner::transformToScan(){
+  initial_points_.clear();
+  geometry_msgs::PointStamped point_l;  // Point in local (scanner) coordinate frame
+  geometry_msgs::PointStamped point_w;  // Point in global (world) coordinate frame
+
+  point_l.header.stamp = ros::Time::now();
+  point_l.header.frame_id = p_scanner_frame_;
+
+  point_w.header.stamp = ros::Time::now();
+  point_w.header.frame_id = p_world_frame_;
+
+  try {
+    tf_listener_.waitForTransform(p_scanner_frame_, p_world_frame_, ros::Time::now(), ros::Duration(3.0));
+
+    for (const Point& point_ : temp_points_) {
+        point_w.point.x = point_.x;
+        point_w.point.y = point_.y;
+        tf_listener_.transformPoint(p_scanner_frame_, point_w, point_l);
+        initial_points_.push_back(Point(point_l.point.x,point_l.point.y));
+      }
+    }
+  }
+  catch (tf::TransformException ex) {
+    ROS_ERROR("%s",ex.what());
+    ros::Duration(1.0).sleep();
+  }
+}
+
+
+void ScanFiner::publishFinedScan(const sensor_msgs::LaserScan::ConstPtr& scan) {
+  sensor_msgs::LaserScan fined_scan_;
+  //std::vector<float> new_arr_;
+  fined_scan_.header.stamp = ros::Time::now();
+  fined_scan_.header.frame_id = p_world_frame_;
+  fined_scan_.angle_min = scan->angle_min;
+  fined_scan_.angle_max = scan->angle_max;
+  fined_scan_.angle_increment = scan->angle_increment;
+  fined_scan_.range_min = scan->range_min;
+  fined_scan_.range_max = scan->range_max;
+
+  for (const Point& point_ : initial_points_){
+    //new_arr_.push_back(point_.length());
+    fined_scan_.ranges.push_back(point_.length());
+  }
+
+  fined_scan_pub_.publish(fined_scan_);
+}
+
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "scan_finer");
+  ScanFiner sf;
+  return 0;
+}
